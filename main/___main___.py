87a81a928f84bfd382dcfd76b13e5c7476e05884
h4t4u <h4t4u.4ever@gmail.com>
2019-09-04 00:52:09 +0300

    final commit, I suppose. It was kinda easy and fun

--- main/___main___.py
+++ main/___main___.py
@@ -1,21 +1,24 @@
 import random
+import subprocess
+import time
 import os
+from termcolor import colored
 
-HEIGHT_CONST = 10
-WIDTH_CONST = 40
+HEIGHT_CONST = 20
+WIDTH_CONST = 60
 field = [0]
 ITEMS = {0: ' ',
          1: '#',
-         2: 's',
-         3: 'F'}
+         2: colored('F', 'blue'),
+         3: colored('s', 'red')}
 
 
 def choose(i):
     if i == 1:
         return 1
-    if i == 2:
+    if i == 2 or i == 4:
         return 2
-    if i == 3:
+    if i == 3 or i == 5:
         return 3
     return 0
 
@@ -23,8 +26,18 @@ def choose(i):
 def fillfield():
     result = [0] * HEIGHT_CONST
     for i in range(HEIGHT_CONST):
-        result[i] = [choose(random.randint(0, 30)) for i in range(WIDTH_CONST)]
+        result[i] = [choose(random.randint(0, 10)) for i in range(WIDTH_CONST)]
     return result
+
+
+def countneighbours(i, j, type):
+    c = 0
+    for k in range(-1, 2):
+        for d in range(-1, 2):
+            if (k != 0 or d != 0) and (0 <= k+i < HEIGHT_CONST and 0 <= j+d < WIDTH_CONST):
+                if field[i+k][j+d] == type:
+                    c = c+1
+    return c
 
 
 def printfield():
@@ -34,6 +47,29 @@ def printfield():
         print()
 
 
+def work():
+    for i in range(HEIGHT_CONST):
+        for j in range(WIDTH_CONST):
+
+            if field[i][j] == 0:
+                if countneighbours(i, j, 2) == 3:
+                    field[i][j] = 2
+                elif countneighbours(i, j, 3) == 3:
+                    field[i][j] = 3
+
+            if field[i][j] == 2:
+                if countneighbours(i, j, 2) < 2 or countneighbours(i, j, 2) > 3:
+                    field[i][j] = 0
+
+            if field[i][j] == 3:
+                if countneighbours(i, j, 3) < 2 or countneighbours(i, j, 3) > 3:
+                    field[i][j] = 0
+
+
 field = fillfield()
 printfield()
-
+while True:
+    printfield()
+    work()
+    time.sleep(1)
+    subprocess.call('clear' if os.name =='posix' else 'cls')
